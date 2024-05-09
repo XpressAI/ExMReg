61e6dcc6f356e63e548eedee3d94b98ae654fd46
Jackson Winslow <jackwinslow35@gmail.com>
2024-05-09 11:06:57 -0400

    Version 0.1 (without dist)

---
+++ src/main/index.js
@@ -0,0 +1,117 @@
+import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron'
+import { join } from 'path'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import icon from '../../resources/icon.png?asset'
+import path from 'path'
+
+import { spawn, ChildProcessWithoutNullStreams } from 'child_process'
+
+function startPythonServer() {
+  const uvicornCommand = 'uvicorn'
+  const args = ['scripts.backend:app', '--host', '127.0.0.1', '--port', '8000']
+  const options = { cwd: path.join(__dirname, '../../') }
+
+  let pythonServer = spawn(uvicornCommand, args, options)
+
+  pythonServer.stdout.on('data', (data) => {
+    console.log(`Python Server: ${data}`)
+  })
+
+  pythonServer.stderr.on('data', (data) => {
+    console.error(`Python Server Error: ${data}`)
+  })
+}
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false
+    }
+  })
+
+  // IPC for file upload
+  // this is needed because from normal js we can't access the full path of the file
+  // Modify the 'open-file-dialog' listener in index.ts to handle specific types of images
+  ipcMain.on('open-file-dialog', async (event, type) => {
+    dialog
+    .showOpenDialog({
+      properties:['openDirectory'],
+    })
+    .then(async (result) => {
+
+        if (!result.canceled && result.filePaths.length > 0) {
+          const filePath = result.filePaths[0]
+          event.reply('file-processed', type, filePath)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
+
+  startPythonServer()
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+
+  // HMR for renderer base on electron-vite cli.
+  // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+
+  // mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  // Set app user model id for windows
+  electronApp.setAppUserModelId('com.electron')
+
+  // Default open or close DevTools by F12 in development
+  // and ignore CommandOrControl + R in production.
+  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  // IPC test
+  ipcMain.on('ping', () => console.log('pong'))
+
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// In this file you can include the rest of your app"s specific main process
+// code. You can also put them in separate files and require them here.
